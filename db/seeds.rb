4d02135b4dd273f775a0d10c9d8126fe12934a91
Neal Wang <vagrant@ubuntu-trusty64>
2016-02-09 00:40:32 +0000

    Added basic book.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Book.destroy_all
+
+book = Book.new
+book.title = 'Last Stop on Market Street'
+book.author = 'Christian Robinson'
+book.description = "Every Sunday after church, CJ and his grandma ride the bus across town. But today, CJ wonders why they don't own a car like his friend Colby. Why doesn’t he have an iPod like the boys on the bus? How come they always have to get off in the dirty part of town? Each question is met with an encouraging answer from grandma, who helps him see the beauty—and fun—in their routine and the world around them."
+book.amazon_id = '0399257748'
+book.rating = 5
+finished_on = 10.days.ago
+book.save
+
+book = Book.new
+book.title = "The War that Saved My Life"
+book.author = "Kimberly Brubaker Bradley"
+book.description = "Nine-year-old Ada has never left her one-room apartment. Her mother is too humiliated by Ada’s twisted foot to let her outside. So when her little brother Jamie is shipped out of London to escape the war, Ada doesn’t waste a minute—she sneaks out to join him.
+ 
+So begins a new adventure of Ada, and for Susan Smith, the woman who is forced to take the two kids in. As Ada teaches herself to ride a pony, learns to read, and watches for German spies, she begins to trust Susan—and Susan begins to love Ada and Jamie. But in the end, will their bond be enough to hold them together through wartime? Or will Ada and her brother fall back into the cruel hands of their mother?
+ 
+This masterful work of historical fiction is equal parts adventure and a moving tale of family and identity—a classic in the making."
+book.amazon_id = "0803740816"
+book.rating = 4
+finished_on = 10.days.ago
+book.save
+
+book = Book.new
+book.title = "Roller Girl"
+book.author = "Victoria Jamieson"
+book.description = "For most of her twelve years, Astrid has done everything with her best friend Nicole. But after Astrid falls in love with roller derby and signs up for derby camp, Nicole decides to go to dance camp instead. And so begins the most difficult summer of Astrid's life as she struggles to keep up with the older girls at camp, hang on to the friend she feels slipping away, and cautiously embark on a new friendship. As the end of summer nears and her first roller derby bout (and junior high!) draws closer, Astrid realizes that maybe she is strong enough to handle the bout, a lost friendship, and middle school… in short, strong enough to be a roller girl.
+
+In this graphic novel debut that earned a Newbery Honor and five starred reviews, real-life derby girl Victoria Jamieson has created an inspiring coming-of-age story about friendship, perseverence, and girl power!"
+book.amazon_id = "0803740166"
+book.rating = 4
+finished_on = 2.days.ago
+book.save
